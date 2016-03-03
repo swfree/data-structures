b4870551a42b640c687e65badb181acf1b999fc6
swfree <skye.free@gmail.com>
2016-03-03 11:06:06 -0800

    Complete LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,33 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // if this is the first node
+      // add it as head to this instance of linkedlist
+    var node = Node(value);
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node; // you don't want to set first node's next to itself
+    }
+    this.tail = node;
   };
 
   list.removeHead = function() {
+    var temp = this.head.value;
+    this.head = this.head.next || null; // if you are removing the only node, head goes back to null
+    return temp;
   };
 
-  list.contains = function(target) {
+  list.contains = function(target, node) {
+    node = node || this.head;
+    if (node.value === target) {
+      return true;
+    }
+    if (this.tail === node) {
+      return false;
+    }
+    return this.contains(target, node.next);
   };
 
   return list;
